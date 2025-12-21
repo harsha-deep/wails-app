a285a416a9186f71a6a4afcc0cf87830e30a03f5
harsha-deep <harshadeep1447@gmail.com>
2025-12-21 19:48:25 +0530

    fresh start

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "System Monitor",
-		Width:  1280,
-		Height: 800,
+		Title:  "wails-app",
+		Width:  1024,
+		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
